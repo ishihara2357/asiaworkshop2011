9d4804030c9a956ea3ee1f7fcf6b478e65c5c549
MNB <2047300+ishihara2357@users.noreply.github.com>
2020-09-19 21:13:11 +0900

    Add files via upload

---
+++ interactive.js
@@ -0,0 +1,29 @@
+﻿$(function () {
+    $(".menu-icon").on("click", function () {
+        // fix the position of menu icon
+        var rightVal = 0;
+        if ($(this).hasClass("open")) {
+
+            // open menu items from right side
+            rightVal = -350;
+
+            // switch menu icon
+            $(this).toggleClass('fa-bars');
+
+            // indicate the menu is alresdy opened
+            $(this).removeClass("open");
+        } else {
+
+            // if class "open" was not given yet (closing state)
+            $(this).addClass("open");
+
+            // switch menu icon
+            $(this).toggleClass('fa-bars');
+        }
+
+        // speed of slide animation 
+        $(".header-right").stop().animate({
+            right: rightVal
+        }, 200);
+    });
+});
